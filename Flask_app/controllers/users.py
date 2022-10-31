3e39e3797e82ece97259647935e4a97f544de2a0
Jake O'Donnell <10jakeo@gmail.com>
2022-10-31 18:04:14 -0400

    Added templates

--- Flask_app/controllers/users.py
+++ Flask_app/controllers/users.py
@@ -1,4 +1,38 @@
 from Flask_app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, session, flash
 from Flask_app.models.user import User
 
+@app.route('/')
+def home():
+    return redirect('/login')
+
+@app.route('/login')
+def r_login():
+    if session:
+        data = session
+    else:
+        data = {
+            'first_name': '',
+            'last_name': '',
+            'email': ''
+        }
+    return render_template('login.html', info = data)
+
+@app.route('register')
+def f_register():
+    # validate info
+    # add to data base
+    # login
+    return redirect('/home')
+
+@app.route('f-login')
+def f_login():
+    # pull account
+    # login
+    return redirect('/home')
+
+@app.route('/logout')
+def logout():
+    # clear session
+    # redirect to home page
+    pass
--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from distutils.log import debug
 from Flask_app import app
 from Flask_app.controllers import users
 
-if __name__ == '__main':
+if __name__ == '__main__':
     app.run(debug = True)
